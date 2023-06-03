1003b8f735a9ac104aa42918a6d92fdb50a05a6d
Yazan Amer <yazan.amer@progressoft.com>
2023-06-03 10:28:06 +0300

    introducing design_targets.py helper classes

--- design_targets.py
+++ design_targets.py
@@ -1,8 +1,20 @@
 import numbers
+from abc import ABCMeta, abstractmethod
+from typing import Union, Tuple
 
 
-class ContinuousTarget:
-    def __init__(self, label, lower_bound, upper_bound):
+class McdTarget(metaclass=ABCMeta):
+    def _validate(self, mandatory_condition: bool, exception_message: str):
+        if not mandatory_condition:
+            raise ValueError(exception_message)
+
+    @abstractmethod
+    def _validate_fields(self):
+        pass
+
+
+class ContinuousTarget(McdTarget):
+    def __init__(self, label: Union[str, int], lower_bound: float, upper_bound: float):
         self.label = label
         self.lower_bound = lower_bound
         self.upper_bound = upper_bound
@@ -15,16 +27,22 @@ class ContinuousTarget:
         self._validate_float(self.upper_bound, "Upper bound")
         self._validate(self.upper_bound > self.lower_bound, "Lower bound cannot be greater or equal to upper bound")
 
-    def _validate_float(self, bound, bound_name):
+    def _validate_float(self, bound, bound_name: str):
         self._validate(isinstance(bound, numbers.Real), f"{bound_name} must be a real number")
 
-    def _validate(self, mandatory_condition, exception_message):
-        if not mandatory_condition:
-            raise ValueError(exception_message)
 
+class ClassificationTarget(McdTarget):
+    def __init__(self, label: Union[str, int], desired_classes: Tuple[Union[str, int]]):
+        self.label = label
+        self.desired_classes = desired_classes
+        self._validate_fields()
 
-class ClassificationTarget:
-    pass
+    def _validate_fields(self):
+        self._validate(isinstance(self.label, (int, str)), "Label must be of type string or an integer index")
+        self._validate(len(str(self.label)) != 0, "Label cannot be an empty string")
+        self._validate(isinstance(self.desired_classes, tuple), "Desired classes must be a tuple")
+        # noinspection PyTypeChecker
+        self._validate(len(self.desired_classes) > 0, "Desired classes cannot be empty")
 
 
 class ProbabilityTarget:
--- tests/test_design_targets.py
+++ tests/test_design_targets.py
@@ -1,27 +1,38 @@
 import unittest
 
-from design_targets import ContinuousTarget
+from design_targets import ContinuousTarget, ClassificationTarget
 
 
 class DesignTargetsTest(unittest.TestCase):
+    # noinspection PyTypeChecker
+    def test_invalid_classification_target(self):
+        self._test_invalid({
+            lambda: ClassificationTarget(None, (50,)): "Label must be of type string or an integer index",
+            lambda: ClassificationTarget("", (50,)): "Label cannot be an empty string",
+            lambda: ClassificationTarget("LABEL", ()): "Desired classes cannot be empty",
+            lambda: ClassificationTarget("LABEL", ["A"]): "Desired classes must be a tuple",
+        })
 
+    # noinspection PyTypeChecker
     def test_invalid_continuous_target(self):
-        factory_to_exception = {
+        self._test_invalid({
             lambda: ContinuousTarget("LABEL", 50, None): "Upper bound must be a real number",
             lambda: ContinuousTarget("LABEL", None, 500): "Lower bound must be a real number",
             lambda: ContinuousTarget(None, 50, 500): "Label must be of type string or an integer index",
             lambda: ContinuousTarget("LABEL", 500, 50): "Lower bound cannot be greater or equal to upper bound",
             lambda: ContinuousTarget("LABEL", 50, 50): "Lower bound cannot be greater or equal to upper bound",
             lambda: ContinuousTarget("", 50, 500): "Label cannot be an empty string",
-        }
-        for factory, exception_message in factory_to_exception.items():
-            with self.subTest():
-                self.assert_raises_with_message(factory, exception_message)
+        })
 
     def test_valid_continuous_target(self):
         ContinuousTarget("LABEL", 50, 500)
         ContinuousTarget(5, 50, 500)
         ContinuousTarget(5, 50.5, 500.15)
+
+    def _test_invalid(self, map_of_cases):
+        for factory, exception_message in map_of_cases.items():
+            with self.subTest(f"Case: {exception_message[:15]} ..."):
+                self.assert_raises_with_message(factory, exception_message)
 
     def assert_raises_with_message(self, faulty_call: callable, expected_message: str):
         with self.assertRaises(ValueError) as context:
