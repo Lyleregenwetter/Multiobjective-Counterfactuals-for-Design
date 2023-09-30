9c609578b88870077a1f837819bed6f9eb61a561
Yazan Amer <yazan.amer@progressoft.com>
2023-09-30 16:31:07 +0300

    remove duplicated code

--- src/decode_mcd/design_targets.py
+++ src/decode_mcd/design_targets.py
@@ -6,22 +6,20 @@ from typing import Union, Sequence, Tuple, List
 import numpy as np
 
 from decode_mcd.mcd_exceptions import UserInputException
+from decode_mcd_private.validation_utils import validate
 
 
 class McdTarget(metaclass=ABCMeta):
-    def _validate(self, mandatory_condition: bool, exception_message: str):
-        if not mandatory_condition:
-            raise UserInputException(exception_message)
 
     @abstractmethod
     def _validate_fields(self):
         pass
 
     def _validate_label(self, label: str):
-        self._validate(isinstance(label, (str, int)),
-                       "Label must be of type string or an integer index")
-        self._validate(len(str(label)) != 0,
-                       "Label cannot be an empty string")
+        validate(isinstance(label, (str, int)),
+                 "Label must be of type string or an integer index")
+        validate(len(str(label)) != 0,
+                 "Label cannot be an empty string")
 
 
 class ContinuousTarget(McdTarget):
@@ -35,10 +33,10 @@ class ContinuousTarget(McdTarget):
         self._validate_label(self.label)
         self._validate_float(self.lower_bound, "Lower bound")
         self._validate_float(self.upper_bound, "Upper bound")
-        self._validate(self.upper_bound > self.lower_bound, "Lower bound cannot be greater or equal to upper bound")
+        validate(self.upper_bound > self.lower_bound, "Lower bound cannot be greater or equal to upper bound")
 
     def _validate_float(self, bound, bound_name: str):
-        self._validate(isinstance(bound, numbers.Real), f"{bound_name} must be a real number")
+        validate(isinstance(bound, numbers.Real), f"{bound_name} must be a real number")
 
 
 class ClassificationTarget(McdTarget):
@@ -49,11 +47,11 @@ class ClassificationTarget(McdTarget):
 
     def _validate_fields(self):
         self._validate_label(self.label)
-        self._validate(isinstance(self.desired_classes, Sequence), "Desired classes must be a sequence")
+        validate(isinstance(self.desired_classes, Sequence), "Desired classes must be a sequence")
         # noinspection PyTypeChecker
-        self._validate(len(self.desired_classes) > 0, "Desired classes cannot be empty")
+        validate(len(self.desired_classes) > 0, "Desired classes cannot be empty")
         for desired_class in self.desired_classes:
-            self._validate(isinstance(desired_class, int), "Desired classes must be an all-integer sequence")
+            validate(isinstance(desired_class, int), "Desired classes must be an all-integer sequence")
 
 
 class ProbabilityTarget(McdTarget):
@@ -66,29 +64,29 @@ class ProbabilityTarget(McdTarget):
     def _validate_fields(self):
         self._validate_sequence(self.labels, "Labels")
         self._validate_sequence(self.preferred_labels, "Preferred labels")
-        self._validate(len(self.labels) > 1, "Labels must have a length greater than 1")
-        self._validate(len(self.preferred_labels) > 0, "Preferred labels cannot be empty")
+        validate(len(self.labels) > 1, "Labels must have a length greater than 1")
+        validate(len(self.preferred_labels) > 0, "Preferred labels cannot be empty")
         self._validate_type_consistency(self.labels, "labels")
         self._validate_type_consistency(self.preferred_labels, "preferred labels")
         self._validate_no_empty_strings(self.labels, "Labels")
         self._validate_no_empty_strings(self.preferred_labels, "Preferred labels")
-        self._validate(set(self.preferred_labels).issubset(self.labels),
-                       "Preferred labels must be a subset of labels")
+        validate(set(self.preferred_labels).issubset(self.labels),
+                 "Preferred labels must be a subset of labels")
 
     def _validate_sequence(self, _object, object_name):
-        self._validate(isinstance(_object, Sequence), f"{object_name} must be a sequence")
+        validate(isinstance(_object, Sequence), f"{object_name} must be a sequence")
 
     def _validate_type_consistency(self, _sequence, sequence_name):
         list_of_types = [type(element) for element in _sequence]
         count_strings = list_of_types.count(str)
         count_integers = list_of_types.count(int)
         exception_message = f"Expected {sequence_name} to be an all-integer or all-string sequence"
-        self._validate((count_integers == 0) or (count_strings == 0), exception_message)
-        self._validate((count_integers + count_strings) == len(list_of_types), exception_message)
+        validate((count_integers == 0) or (count_strings == 0), exception_message)
+        validate((count_integers + count_strings) == len(list_of_types), exception_message)
 
     def _validate_no_empty_strings(self, _sequence, sequence_name):
         lengths = [len(str(element)) for element in _sequence]
-        self._validate(0 not in lengths, f"{sequence_name} cannot contain empty strings")
+        validate(0 not in lengths, f"{sequence_name} cannot contain empty strings")
 
 
 class DesignTargets:
@@ -120,25 +118,25 @@ class DesignTargets:
         self._validate_sequence(self.classification_targets, "Classification targets", ClassificationTarget)
         self._validate_sequence(self.probability_targets, "Probability targets", ProbabilityTarget)
         self._validate_no_crossover()
-        self._validate(self.count_constrained_labels() > 0, "Design targets must be provided")
+        validate(self.count_constrained_labels() > 0, "Design targets must be provided")
 
     def _validate(self, mandatory_condition: bool, exception_message: str):
         if not mandatory_condition:
             raise UserInputException(exception_message)
 
     def _validate_sequence(self, _sequence, _sequence_name, element_type):
-        self._validate(isinstance(_sequence, Sequence), f"{_sequence_name} must be a sequence")
+        validate(isinstance(_sequence, Sequence), f"{_sequence_name} must be a sequence")
         for element in _sequence:
-            self._validate(isinstance(element, element_type),
-                           f"{_sequence_name} must be composed of elements of class {element_type.__name__}")
+            validate(isinstance(element, element_type),
+                     f"{_sequence_name} must be composed of elements of class {element_type.__name__}")
 
     def _validate_no_crossover(self):
         continuous = set(self.get_continuous_labels())
         classification = set(self.get_classification_labels())
         probability = set(self._get_probability_labels())
         len_union = len((continuous | classification) | probability)
-        self._validate(self.count_constrained_labels() == len_union,
-                       "Label was specified twice in targets")
+        validate(self.count_constrained_labels() == len_union,
+                 "Label was specified twice in targets")
 
     def get_continuous_boundaries(self) -> Tuple[np.ndarray, np.ndarray]:
         lower_bounds = [target.lower_bound for target in self.continuous_targets]
