bugfix: dataset_subset is empty, causing nans in average gower
set_valid_datasets_subset now builds a new object belonging to the data_package called valid_features_dataset rather than updating features_dataset.

src/decode_mcd/multi_objective_problem.py
@@ -108,7 +108,7 @@ class MultiObjectiveProblem(Problem):
         all_scores[:, :-_MCD_BASE_OBJECTIVES] = predictions.loc[:, self._data_package.bonus_objectives]
         all_scores[:, _GOWER_INDEX] = mixed_gower(x, self._data_package.query_x, self._ranges.values, gower_types).T
         all_scores[:, _CHANGED_FEATURE_INDEX] = changed_features_ratio(x, self._data_package.query_x,
-                                                                       len(self._data_package.features_dataset.columns))
+                                                                       len(self._data_package.valid_features_dataset.columns))
         subset = self._get_features_sample()
         all_scores[:, _AVG_GOWER_INDEX] = avg_gower_distance(x, subset, self._ranges.values, gower_types)
         return all_scores
@@ -145,7 +145,7 @@ class MultiObjectiveProblem(Problem):
             # TODO: test this!
             p_d = p_d[np.equal(f_d_view.values, query_view.values).all(axis=1)]
             f_d = f_d[np.equal(f_d_view.values, query_view.values).all(axis=1)]
-        self._data_package.features_dataset = f_d
+        self._data_package.valid_features_dataset = f_d
         self._data_package.predictions_dataset = p_d
 
     def _get_valid_categorical_entries(self, f_d, p_d):
@@ -275,7 +275,7 @@ class MultiObjectiveProblem(Problem):
         df = pd.concat([self._data_package.query_x] * n, axis=0, )
         df.index = list(range(n))
         df = pd.concat([df.loc[:, self._data_package.features_to_freeze], x], axis=1)
-        df = df[self._data_package.features_dataset.columns]
+        df = df[self._data_package.valid_features_dataset.columns]
         return df
 
     def _validate(self, mandatory_condition, error_message):